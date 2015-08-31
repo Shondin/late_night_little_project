c342c00e0d25e7b453629c3ebc64a59050bbe41c
Шондин Дмитрий <ds@calculate.ru>
2015-08-31 17:53:53 +0300

    test file for getting test time

--- main/js/main.js
+++ main/js/main.js
@@ -1,23 +1,25 @@
  $(function(){
-        var $pep = $('.pep');
-        var $matrix = $('#matrix');
+        var pep_el = document.getElementById('pep');
+        var matrix_el =document.getElementById("matrix");
 
         var now = new Date();
 
-        var pep_width = window.innerWidth*6; // ширина таймлайна - 6 экранов
-            $pep.css('width',pep_width+'px');
+        var pep_width = window.innerWidth*2; // ширина таймлайна - 6 экранов
+            pep_el.style.width = pep_width+'px';
         var minutes_img_width = 10;
         var minutes = (pep_width/minutes_img_width)/2;
         var start_time = parseInt((now.getHours() - minutes/60));
 
         //cur time
         var id; var clas;
-        var time_html; var min; var hour ;
+        var span; var min; var hour ;
             minutes *= 2;
+        var div = document.createElement("div");
         for (var i =0; i<=minutes; i++){
             id ="";
             clas ="";
-            time_html = ""
+            span = null;
+            var e = document.createElement("div");
             if(i>60){
                 min = i%60;
             }else{
@@ -28,21 +30,26 @@
             if(i%5==0){
                 id="id='"+parseInt(hour+i/60)+"_"+min+"'";
                 if(min<10) min = "0"+min;
-                time_html = "<span class='time'>" + hour+":"+min+"</span>";
+                span = document.createElement("span");
+                span.className = "time"
+                span.innerHTML = hour+":"+min
                 clas = clas+" l"
             }
+            e.id = id;
+            e.className="col "+clas;
+            e.style.width =minutes_img_width;
+            matrix_el.appendChild(e);
+            if(span!=null) matrix_el.appendChild(span);
 
-            $matrix.append("<div "+id+" class='col"+clas+"' style='width:"+minutes_img_width+"px'></div>"+time_html);
         }
 
 
-        $('.pep').pep({
+        $(pep_el).pep({
               axis: 'x',
               //constrainTo: [0, 6000, 0, -3000],
 
               initiate: function(ev, obj) {},
-              start: function(ev, obj)    {
-              },
+              start: function(ev, obj)    {},
               drag: function(ev, obj)     {},
               stop: function(ev, obj)     {},
               rest: function(ev, obj)     {}
