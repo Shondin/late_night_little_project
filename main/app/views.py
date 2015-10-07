214dc463ab9baff67064c1e51757977a9677df77
Шондин Дмитрий <ds@calculate.ru>
2015-10-07 17:33:17 +0300

    install login,migration modules for  flask

--- main/app/views.py
+++ main/app/views.py
@@ -1,5 +1,8 @@
-from app import app
+
 from flask import render_template, json, jsonify
+from flask_admin import admin
+import flask_login as login
+from werkzeug.security import generate_password_hash, check_password_hash
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -27,3 +30,79 @@ def get_json_from_file(json_url):
     data = {}
     data['data'] = json.load(open(json_url))
     return jsonify(data)
+
+# Initialize flask-login
+def init_login():
+    login_manager = login.LoginManager()
+    login_manager.init_app(app)
+
+    # Create user loader function
+    @login_manager.user_loader
+    def load_user(user_id):
+        return db.session.query(User).get(user_id)
+
+
+# Create customized model view class
+class MyModelView(sqla.ModelView):
+
+    def is_accessible(self):
+        return login.current_user.is_authenticated()
+
+
+# Create customized index view class that handles login & registration
+class MyAdminIndexView(admin.AdminIndexView):
+
+    @expose('/')
+    def index(self):
+        if not login.current_user.is_authenticated():
+            return redirect(url_for('.login_view'))
+        return super(MyAdminIndexView, self).index()
+
+    @expose('/login/', methods=('GET', 'POST'))
+    def login_view(self):
+        # handle user login
+        form = LoginForm(request.form)
+        if helpers.validate_form_on_submit(form):
+            user = form.get_user()
+            login.login_user(user)
+
+        if login.current_user.is_authenticated():
+            return redirect(url_for('.index'))
+        link = '<p>Don\'t have an account? <a href="' + url_for('.register_view') + '">Click here to register.</a></p>'
+        self._template_args['form'] = form
+        self._template_args['link'] = link
+        return super(MyAdminIndexView, self).index()
+
+    @expose('/register/', methods=('GET', 'POST'))
+    def register_view(self):
+        form = RegistrationForm(request.form)
+        if helpers.validate_form_on_submit(form):
+            user = User()
+
+            form.populate_obj(user)
+            # we hash the users password to avoid saving it as plaintext in the db,
+            # remove to use plain text:
+            user.password = generate_password_hash(form.password.data)
+
+            db.session.add(user)
+            db.session.commit()
+
+            login.login_user(user)
+            return redirect(url_for('.index'))
+        link = '<p>Already have an account? <a href="' + url_for('.login_view') + '">Click here to log in.</a></p>'
+        self._template_args['form'] = form
+        self._template_args['link'] = link
+        return super(MyAdminIndexView, self).index()
+
+    @expose('/logout/')
+    def logout_view(self):
+        login.logout_user()
+        return redirect(url_for('.index'))
+
+
+# Flask views
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
--- main/run.py
+++ main/run.py
@@ -1,5 +1,17 @@
 from app import app
-from flask_admin import Admin
+from app.views import MyAdminIndexView, MyModelView
+import flask_admin as admin
+
 admin = Admin(app, name='microblog', template_mode='bootstrap3')
+# Initialize flask-login
+init_login()
+
+# Create admin
+admin = admin.Admin(app, 'Example: Auth', index_view=MyAdminIndexView(), base_template='my_master.html')
+
+# Add view
+admin.add_view(MyModelView(User, db.session))
+
+
 # Add administrative views here
 app.run(debug=True)
